f2387f5524f74caee0603daae3fea8ef2dbfbc34
Lawftyg <mymsnadressis@hotmail.com>
2025-08-14 13:58:20 +0200

    fudge around and find out

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,34 @@
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+import { gameInitialState } from './state/game-state'
+
+const gameState = gameInitialState;
+
+const textFields = ["day"];
+
+textFields.forEach((field: string) => {
+  const element = document.getElementById(field);
+  (element as HTMLElement).innerText = gameState[field as keyof typeof gameState].toString();
+})
+
+const updateDayButton = document.getElementById("advanceDay");
+
+updateDayButton!.onclick = updateDay;
+
+
+
+
+
+export function updateDay() {
+  gameState.day += 1;
+  const dayElement = document.getElementById("day");
+  dayElement && (dayElement.innerText = gameState.day.toString());
+
+
+}
+
+
+
+
+
