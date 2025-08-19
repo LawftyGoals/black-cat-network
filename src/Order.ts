4dc04d509eefbc43f1f2e4f03f8f2f49a6ec08e1
Lawftyg <mymsnadressis@hotmail.com>
2025-08-19 15:18:56 +0200

    remove old generateelement and rename new one

--- src/Order.ts
+++ src/Order.ts
@@ -35,82 +35,15 @@ export function updateOrdersElement() {
 
         clearChildren(orderElement);
 
-        orders.forEach((order, id) => {
-            orderElement.appendChild(generateStreamlinedOrderElement(order, id));
+        orders.forEach((order) => {
+            orderElement.appendChild(generateOrderElement(order));
 
         })
 
     }
 }
 
-export function generateOrderElement(order: Order, id: string) {
-    const orderLi = cE("div");
-    orderLi.id = "order-" + id;
-
-    const fromElement = cE("p");
-    fromElement.id = "order-from-" + id;
-    fromElement.innerText = "From: " + order.From;
-
-    orderLi.appendChild(fromElement);
-
-    const descriptionElement = cE("p");
-    descriptionElement.id = "order-from-" + id;
-    descriptionElement.innerText = "Description: " + order.Description;
-    orderLi.appendChild(descriptionElement);
-
-    const offerElement = cE("p");
-    offerElement.id = "order-from-" + id;
-    offerElement.innerText = "Offering: " + order.Offer.toString() + "gp";
-    orderLi.appendChild(offerElement);
-
-    const requirementsDL = cE("dl");
-    const requirementsDT = cE("dt");
-
-    requirementsDT.innerText = "Requirements:";
-    requirementsDL.appendChild(requirementsDT)
-
-    const variantDD = cE("dd");
-    variantDD.innerHTML = `<strong>${variantMapping[order.Variant]}</strong>`;
-    requirementsDT.appendChild(variantDD);
-
-    order.Requirements.forEach((requirement) => {
-        const requirementDD = cE("dd");
-        requirementDD.innerText = characteristicsMapping[(requirement as TenumCatCharacteristics)];
-        requirementsDT.appendChild(requirementDD);
-    })
-
-    orderLi.appendChild(requirementsDL);
-
-    const completeButton = cE("button");
-    completeButton.innerText = "Complete Order";
-    completeButton.onclick = () => {
-
-        setSelectedOrder(order);
-
-        const dialog = gEiD("dialog") as HTMLDialogElement;
-        dialog?.showModal();
-
-
-        const closeButton = gEiD("close-dialog");
-
-        closeButton!.onclick = () => {
-            clearSelectedOrder();
-            clearSelectedCat();
-            dialog.close();
-
-        };
-    };
-
-    orderLi.appendChild(completeButton);
-
-
-    return orderLi
-}
-
-
-
-
-export function generateStreamlinedOrderElement(order: Order, id: string) {
+export function generateOrderElement(order: Order) {
     const orderDiv = cE("div");
     for (const [key, value] of Object.entries(order)) {
 
