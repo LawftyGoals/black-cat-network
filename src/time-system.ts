3c4e9fe9f2037532027514686e4f9a53237f4059
Lawftyg <mymsnadressis@hotmail.com>
2025-08-21 23:36:59 +0200

    woopsie

--- src/time-system.ts
+++ src/time-system.ts
@@ -3,22 +3,22 @@ import { gameInitialState } from "./state/game-state";
 const gameState = gameInitialState;
 
 export function getTime() {
-    return {
-        remainingTime: gameState.remainingTime,
-        maxTime: gameState.maxTime
-    };
+  return {
+    remainingTime: gameState.remainingTime,
+    maxTime: gameState.maxTime,
+  };
 }
 
 export function changeRemainingTime(change: number = -1) {
-    gameState.remainingTime += change;
-    return gameState.remainingTime;
+  gameState.remainingTime += change;
+  return gameState.remainingTime;
 }
 
 export function changeMaxTime(change: number = -1) {
-    gameState.maxTime += change;
-    return gameState.maxTime;
+  gameState.maxTime += change;
+  return gameState.maxTime;
 }
 
 export function resetRemainingTime() {
-    gameState.remainingTime = gameState.maxTime;
+  gameState.remainingTime = gameState.maxTime;
 }
